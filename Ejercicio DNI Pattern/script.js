d66caea7dfd8c2e07a0d2b3c5983b29527e3c41c
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-02-02 14:57:52 +0100

    ejercicio

--- Ejercicio DNI Pattern/script.js
+++ Ejercicio DNI Pattern/script.js
@@ -12,6 +12,9 @@ let pattern = /([0-9]{1,8})(\-{0,1})([a-z]{1})/gi;
 let patternNumero = /[0-9]{1,8}/gi;
 let patternLetra = /[a-z]{1}/gi;
 
+let texto = "Hola mundo JavaScript con reduce y metodos de array!!";
+let arrayResultado = [];
+
 function ejecutaDNI () {
     let dni = nodoDNI.value;
     if (pattern.test(dni)) {
@@ -34,3 +37,42 @@ function sema () {
             nodoResultado.innerHTML = "La letra introducida no se corresponde con el número."
         }
 }
+
+function cuentaLetras () {
+
+    for (let i = 0; i < texto.length; i++) {
+        let letra = texto[i];
+        if (!arrayResultado.includes(letra)) {
+            arrayResultado.push(letra);
+        }
+    }
+    let nuevo = arrayResultado.map( (cadaLetra) => {
+        return {letra: cadaLetra, numero: 0};
+    });
+    nuevo.forEach( (cadaObjeto) => {
+        for (let i = 0; i < texto.length; i++) {
+            if (cadaObjeto.letra === texto[i]) {
+                cadaObjeto.numero++;
+            }
+        }
+    });
+    console.log(nuevo);
+}
+
+function cuentaLetrasFilter () {
+    let resultado = texto.filter( (letra) => {
+        
+    });
+}
+
+cuentaLetras();
+
+
+
+/*
+{
+    a:10,
+    b:2,
+}
+
+*/
