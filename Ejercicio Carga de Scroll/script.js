704f173e0958801fd1691376c99536e0410911a8
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-02-10 12:28:31 +0100

    lol

--- Ejercicio Carga de Scroll/script.js
+++ Ejercicio Carga de Scroll/script.js
@@ -6,8 +6,8 @@ $(window).on({
         let bodyHeight = Math.round($('body').height());
         let windowHeight = Math.round($(window).height());
         
-        let cosas = bodyHeight - windowHeight;
-        let porcentajeWidth = Math.trunc((top_scroll / cosas) * 100);
+        let limiteHeight = bodyHeight - windowHeight;
+        let porcentajeWidth = Math.trunc((top_scroll / limiteHeight) * 100);
 
         $('#barra').css({
             'width' : `${porcentajeWidth}%`
