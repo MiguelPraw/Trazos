05d6949c1d026e76d69372f31343d85f53cfbb48
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-02-10 12:27:57 +0100

    lol

--- Ejercicio Carga de Scroll/script.js
+++ Ejercicio Carga de Scroll/script.js
@@ -2,19 +2,16 @@
 
 $(window).on({
     scroll: function () {
-        console.clear();
         let top_scroll = Math.round($(this).scrollTop());
         let bodyHeight = Math.round($('body').height());
         let windowHeight = Math.round($(window).height());
-        let porcentajeWidth = Math.trunc(((top_scroll + windowHeight) / bodyHeight) * 100);
-        console.log(top_scroll);
-        console.log(windowHeight);
-        console.log(bodyHeight);
-        console.log("Relacion TOP / BODY:", top_scroll / bodyHeight);
-        console.log("Relacion");
-        console.log("Distancia desde el top hasta abajo del window", top_scroll + windowHeight);
+        
+        let cosas = bodyHeight - windowHeight;
+        let porcentajeWidth = Math.trunc((top_scroll / cosas) * 100);
+
         $('#barra').css({
             'width' : `${porcentajeWidth}%`
         });
     }
 });
+
