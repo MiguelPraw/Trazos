a2bc01a79e639dfe4b27b5a7003ea31f237fc800
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-03-02 14:37:32 +0100

    prueba Angular

--- Angular/Hola-Mundo/src/app/app.component.ts
+++ Angular/Hola-Mundo/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hola-Mundo';
+  public title:string = 'Hola-Mundo';
+  public nombre:string = 'Jacinto';
+  public contador:number = 0;
+
+  constructor () {
+    //this.title = "PEPE" + 9;
+  }
+
+  public go_prev () {
+    console.log("Prev");
+    this.contador--;
+  }
+
+  public go_next () {
+    console.log("Next");
+    this.contador++;
+  }
+
 }
