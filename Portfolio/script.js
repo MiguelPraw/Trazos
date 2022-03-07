d0a4259faa7a0792519d0a98492b8e5872483efb
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-03-07 23:57:53 +0100

    k

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,11 +1,12 @@
 'use strict'
 
-let nodoPortada = document.querySelector('.portada');
+/*let nodoPortada = document.querySelector('.portada');
 let nodoH1 = document.querySelector('.portada__h1');
 
 let texto = "Hola, soy Miguel Pérez, desarrollador web.";
 
 let n = 0;
+
 
 let textoPrincipal = setInterval( function (){
 
@@ -16,21 +17,41 @@ let textoPrincipal = setInterval( function (){
         clearInterval(textoPrincipal);
     }
 
-} , 100);
+}, 100);*/
+
+let distancia = 0;
+let nodoMainActivo;
+
+document.querySelector('body').addEventListener ('wheel', function (evento) {
+    console.log(evento);
+    console.log(nodoMainActivo);
+    if (evento.deltaY > 0) {
+        if (distancia <= 200) {
+            distancia += 100;
+        }
+    } else {
+        if (distancia >= 100) {
+            distancia -= 100;
+        }
+    }
+    añadeClaseHeader ();
+    actualizaMain ();
+    document.querySelector('main').style.transform = `translateY(-${distancia}%)`;
+});
+
+function añadeClaseHeader () {
+    let nodoLi = document.querySelectorAll('ul li');
+    nodoLi.forEach ( li => {
+        li.classList.remove('activo');
+        
+    });
+    nodoLi[distancia/100].classList.add('activo');
+}
+
+function actualizaMain () {
+    let nodoMain = document.querySelectorAll('main>div');
+    nodoMainActivo = nodoMain[distancia/100];
+    console.log(nodoMainActivo);
+}
 
 
-// nodoJuego.addEventListener ('mousemove', (evento) => {
-
-//     let ejeX = (evento.pageX - window.innerWidth / 2) / 25;
-//     let ejeY = (window.innerHeight / 2 - evento.pageY) / 25;
-
-//     console.log("X", ejeX);
-//     console.log ("Y", ejeY);
-
-//     nodoJuego.style.transform = `rotateY(${ejeX}deg) rotateX(${ejeY}deg)`;
-// });
-
-// nodoJuego.addEventListener ('mouseleave', (evento) => {
-//     nodoJuego.style.transition = 'transform 0.5s ease ';
-//     nodoJuego.style.transform = 'rotateY(0 deg) rotateX(0 deg)';
-// });
