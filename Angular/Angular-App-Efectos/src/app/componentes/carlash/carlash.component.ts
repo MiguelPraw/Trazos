25528399acdca086b9e929420b271a084892b1d2
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-03-24 22:08:45 +0100

    fgfgf

--- Angular/Angular-App-Efectos/src/app/componentes/carlash/carlash.component.ts
+++ Angular/Angular-App-Efectos/src/app/componentes/carlash/carlash.component.ts
@@ -8,20 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class CarlashComponent implements OnInit {
 
   items : any[] = [
-    { nombre : "ZPump Urban Fit" , marca : "Reebok" },
-    { nombre : "Coach2o" , marca : "Font Vella" }, 
-    { nombre : "RCar" , marca : "Revista Car" }, 
-    { nombre : "Emotional Pulse" , marca : "Seidor" }, 
-    { nombre : "Alhambra" , marca : "Cervezas Alhambra" }, 
-    { nombre : "Actimel" , marca : "Danone" }, 
-    { nombre : "ffuuss" , marca : "OT'S Group" }, 
-    { nombre : "Delicious" , marca : "Día" }, 
-    { nombre : "Roca" , marca : "Sanitarios" }, 
-    { nombre : "Deep State" , marca : "Fox" }
-  ]
+    { nombre : "ZPump Urban Fit"  , marca : "Reebok"            , fondo : "../../assets/zpump.jpg"            },
+    { nombre : "Coach2o"          , marca : "Font Vella"        , fondo : "../../assets/coach2o.jpg"          }, 
+    { nombre : "RCar"             , marca : "Revista Car"       , fondo : "../../assets/coche.jpg"            }, 
+    { nombre : "Emotional Pulse"  , marca : "Seidor"            , fondo : "../../assets/señor.jpg"            }, 
+    { nombre : "Alhambra"         , marca : "Cervezas Alhambra" , fondo : "../../assets/cervezaAlhambra.jpg"  }, 
+    { nombre : "Actimel"          , marca : "Danone"            , fondo : "../../assets/actimel.jpg"          }, 
+    { nombre : "ffuuss"           , marca : "OT'S Group"        , fondo : "../../assets/movil.jpg"            }, 
+    { nombre : "Delicious"        , marca : "Día"               , fondo : "../../assets/DElicious.jpg"        }, 
+    { nombre : "Roca"             , marca : "Sanitarios"        , fondo : "../../assets/roca.jpg"             }, 
+    { nombre : "Deep State"       , marca : "Fox"               , fondo : "../../assets/deepState.jpg"        }
+  ];
+
+  actual : number = -1;
 
   constructor() { }
 
   ngOnInit(): void { }
+
+  setActual( indice : number ) : void {
+    this.actual = indice;
+  }
 
 }
