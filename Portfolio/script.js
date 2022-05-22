14d4453b78990a662848be5cb9bc852986219706
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-05-22 23:52:36 +0200

    portfolio

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -37,7 +37,6 @@ document.querySelector('.main__portada').addEventListener( 'mousemove' , functio
 /***** JS para el SCROLL *****/
 
 let distancia = 0;
-let nodoMainActivo;
 
 document.querySelector('body').addEventListener ('wheel', ( evento ) => {
     if (evento.deltaY > 0) {
@@ -58,6 +57,9 @@ let nodoH2React = document.querySelector('.proyectos__h2.react');
 let nodoH2Angular = document.querySelector('.proyectos__h2.angular');
 let nodoH2Javascript = document.querySelector('.proyectos__h2.javascript');
 
+let nodosSpecs = document.querySelectorAll('.proyectos__specs');
+let nodosIconos = document.querySelectorAll('.proyectos__icono');
+
 let nodoGridAngular = document.querySelector('.proyectos__grid.angular');
 let nodoGridReact = document.querySelector('.proyectos__grid.react');
 let nodoGridJavascript = document.querySelector('.proyectos__grid.javascript');
@@ -70,6 +72,8 @@ document.querySelectorAll('.proyectos__h2').forEach( nodoProyecto => {
             nodoH2React.classList.remove('activo');
             nodoH2Javascript.classList.remove('activo');
 
+            nodosSpecs.forEach( nodo => nodo.classList.remove('activo'));
+            nodosIconos.forEach( nodo => nodo.classList.contains('angular') ? nodo.classList.add('activo') : nodo.classList.remove('activo') );
             this.nextElementSibling.classList.add('activo');
 
             nodoGridAngular.classList.add('activo');
@@ -80,6 +84,10 @@ document.querySelectorAll('.proyectos__h2').forEach( nodoProyecto => {
             nodoH2Angular.classList.remove('activo');
             nodoH2Javascript.classList.remove('activo');
 
+            nodosSpecs.forEach( nodo => nodo.classList.remove('activo'));
+            nodosIconos.forEach( nodo => nodo.classList.contains('react') ? nodo.classList.add('activo') : nodo.classList.remove('activo') );
+            this.nextElementSibling.classList.add('activo');
+
             nodoGridReact.classList.add('activo');
             nodoGridAngular.classList.remove('activo');
             nodoGridJavascript.classList.remove('activo');
@@ -87,6 +95,10 @@ document.querySelectorAll('.proyectos__h2').forEach( nodoProyecto => {
         } else if (this.classList.contains('javascript')) {
             nodoH2Angular.classList.remove('activo');
             nodoH2React.classList.remove('activo');
+
+            nodosSpecs.forEach( nodo => nodo.classList.remove('activo'));
+            nodosIconos.forEach( nodo => nodo.classList.contains('javascript') ? nodo.classList.add('activo') : nodo.classList.remove('activo') );
+            this.nextElementSibling.classList.add('activo');
 
             nodoGridJavascript.classList.add('activo');
             nodoGridAngular.classList.remove('activo');
